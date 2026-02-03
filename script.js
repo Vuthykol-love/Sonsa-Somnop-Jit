2c9e721d206c467eaef5a4fc34f20bf3aa19ef59
Vuthykol-love <koolkid221307@gmail.com>
2026-02-03 11:07:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+yesYay = document.getElementById('yesBtn')
+noNay = document.getElementById('noBtn')
+function alertfunc(){
+    alert("YESSSSSS MY VALENTINEEEE <3")
+}
+
+function removeBtn(){
+     noNay.removeAttribute('onclick')
+}
